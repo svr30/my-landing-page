bc4287f684071e246ca4fdcbab26b4843589222a
svr30 <145685135+svr30@users.noreply.github.com>
2024-06-14 15:55:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('#hero button');
+    button.addEventListener('click', () => {
+        alert('Button clicked!');
+    });
+});
+
